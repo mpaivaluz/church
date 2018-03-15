2edebac9878771be6eb65ba329e08a9b17eeecb9
Miguel Luz <37408479+mpaivaluz@users.noreply.github.com>
2018-03-15 14:42:02 -0300

    Esqueceu senha

--- enviaresqueceusenha.php
+++ enviaresqueceusenha.php
@@ -8,8 +8,14 @@ $row = mysqli_num_rows($query);
 if (($row)=='0'){
 		echo "<script>window.location='esqueceusenha.php';alert('Email não existe');</script>";
 } else {
-
+$subject = "Link para alteração de email";
+$message = "<p>Clique para alterar a senha <a href='localhost/Projeto/enviarnovasenha.php'>aqui</a></p>"; 
+//",Bem Vindo(a)";
+$headers = "To: <$email>" . "\r\n";
+$headers .= "From: Teste <mpaivaluz@gmail.com>" . "\r\n";
+$headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
+mail($email, $subject, $message, $headers);
+echo "<script>window.location='index.php';alert('email enviado com sucesso');</script>";
 }
-
 
 ?>
