602ed0959539f29cc968243d20bf6256432278c1
Miguel Luz <37408479+mpaivaluz@users.noreply.github.com>
2018-03-15 14:01:01 -0300

    criando esqueceu senha

--- enviaresqueceusenha.php
+++ enviaresqueceusenha.php
@@ -5,10 +5,10 @@ $db = mysqli_select_db($con, "company");
 $sql = "SELECT email from login where email='$email'";
 $query = mysqli_query($con, $sql);
 $row = mysqli_num_rows($query);
-if (($row)=='1'){
-		
+if (($row)=='0'){
+		echo "<script>window.location='esqueceusenha.php';alert('Email não existe');</script>";
 } else {
-	echo "<script>window.location='esqueceusenha.php';alert('Email não existe');</script>";
+
 }
 
 
---
+++ novasenha.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Cadastro</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+<script charset="UTF-8" type="text/javascript" src="JS/script.js">
+
+</script>
+</head>
+<body>
+<form id="formCadastro" name="formulario" action="enviarnovasenha.php" method="POST">
+					<label for="txtSenha"><h1 class="senha">Nova Senha</h1></label>
+                    <input id="txtSenha" name="password" type="password" required placeholder="Digite uma Senha" title="Senha" /> <br><br>
+                    <label for="repetir_senha"><h1 class="senha">Confirmar Nova Senha</h1></label>
+                    <input id="repetir_senha" name="repetir_senha" type="password" required  placeholder="Repetir Senha" title="Repetir Senha" oninput="validaSenha(this)" /> <br> <br>
+                    <button type="submit" class="enviar">Enviar</button>
+    </form>
+</body>
+</html>
