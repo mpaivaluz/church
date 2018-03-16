e92a11d4fbc0aaeb950de38689e3f371b9b17661
Miguel Luz <37408479+mpaivaluz@users.noreply.github.com>
2018-03-16 09:50:55 -0300

    Noca tela de login

--- cadastro.php
+++ cadastro.php
@@ -31,33 +31,7 @@ header("location: profile.php");
                     <input id="repetir_senha" name="repetir_senha" type="password" required  placeholder="Repetir Senha" title="Repetir Senha" oninput="validaSenha(this)" /> <br> <br>
                     <button type="submit" class="enviar">Enviar</button>
     </form>
-    <div class="box-parent-login">
-	<div class="well bg-white box-login">
-		<h1 class="ls-login-logo">locaweb</h1>
-		<form role="form">
-			<fieldset>
- 
-				<div class="form-group ls-login-user">
-					<label for="userLogin">Usuário</label>
-					<input class="form-control ls-login-bg-user input-lg" id="userLogin" type="text" aria-label="Usuário" placeholder="Usuário">
-				</div>
- 
-				<div class="form-group ls-login-password">
-					<label for="userPassword">Senha</label>
-					<input class="form-control ls-login-bg-password input-lg" id="userPassword" type="password" aria-label="Senha" placeholder="Senha">
-				</div>
- 
-				<a href="#" class="ls-login-forgot">Esqueci minha senha</a>
- 
-				<input type="submit" value="Entrar" class="btn btn-primary btn-lg btn-block">
-				<p class="txt-center ls-login-signup">Não possui um usuário na Locaweb?
-					<a href="#">Cadastre-se agora</a>
-				</p>
- 
-			</fieldset>
-		</form>
-	</div>
-</div>
+    
 </body>
 </html>
 
